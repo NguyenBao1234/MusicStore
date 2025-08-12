a77fdd60852207e54b7bd0d11d5741874f8dcd02
NguyenBao1234 <robertpamvn@gmail.com>
2025-08-12 10:28:23 +0700

    - add empty user control into Dialog Window for presentation ui

---
+++ Views/MusicStoreViewUC.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MusicStore.Views;
+
+public partial class MusicStoreViewUC : UserControl
+{
+    public MusicStoreViewUC()
+    {
+        InitializeComponent();
+    }
+}
