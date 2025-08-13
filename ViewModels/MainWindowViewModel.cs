973afec51832b96334e0f68693080e66fc03c54f
NguyenBao1234 <robertpamvn@gmail.com>
2025-08-13 23:01:40 +0700

    - Load Bought Albums On Main Window View when open App

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using MusicStore.Message;
+using MusicStore.Models;
 
 namespace MusicStore.ViewModels;
 
 public partial class MainWindowViewModel : ObservableObject
 {
-    public ObservableCollection<AlbumViewModel> Albums { get; } = new();
+    public ObservableCollection<AlbumViewModel> AlbumVMs { get; } = new();
     public MainWindowViewModel()
     {
         // ViewModel initialization logic.
+        LoadAlbums();
     }
 
     [RelayCommand]
@@ -22,8 +25,26 @@ public partial class MainWindowViewModel : ObservableObject
         AlbumViewModel? album = await WeakReferenceMessenger.Default.Send(new PurchaseAlbumMessage());
         if (album != null)
         {
-            Albums.Add(album);
+            AlbumVMs.Add(album);
             await album.SaveToDiskAsync();
         }
     }
+    
+    private async void LoadAlbums()
+    {
+        var albums = await Album.LoadCachedAsync();
+        foreach (var album in albums)
+        {
+            AlbumVMs.Add(new AlbumViewModel(album));
+        }
+
+        // foreach (var album in AlbumVMs)
+        // {
+        //     await album.LoadCover();//Load one by one
+        // }
+        
+        var coverTasks = AlbumVMs.Select(album => album.LoadCover());//Load all parallel
+        await Task.WhenAll(coverTasks);
+    }
+
 }
