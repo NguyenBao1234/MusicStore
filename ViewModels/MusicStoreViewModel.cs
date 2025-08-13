80f19bbb591aad3a2b778eda227ce8e625ba1c37
NguyenBao1234 <robertpamvn@gmail.com>
2025-08-13 13:54:51 +0700

    - Mock Data for search (only for test presentation )

--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,20 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace MusicStore.ViewModels;
 
-public class MusicStoreViewModel
+public partial class MusicStoreViewModel:ViewModelBase
 {
-    
+    [ObservableProperty] public partial string? SearchText { get; set; }
+        
+    [ObservableProperty] public partial bool IsBusy { get; private set; }
+    [ObservableProperty] public partial AlbumViewModel? SelectedAlbum { get; set; }
+
+    public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();// <=> [] in c#12; this var like List<> or Array<>
+    public MusicStoreViewModel()
+    {
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+    }
 }
